dff342da33c992edb65776f21e815e87c5312eac
Cheyenne Arrowsmith <dinophile@users.noreply.github.com>
2019-10-07 19:29:37 -0400

    Update main_test.go

--- app/main/main_test.go
+++ app/main/main_test.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"testing"
+	"github.com/dinophile/routes"
 )
 
 // TestHealth checks app health
+// So I wrote this test back when I wasn't exporting my routes, and health was just a function within main
+// And here's where I hit a wall, dig into http request testing or hand in the tech test?
+// TODO: continue to do some research and will add once I learn more!
+// Usually with a larger project I like to map out my test cases and do TDD when I'm more comfortable, but in this case 
+// I rushed to implement the main program as a priority!
+
 func TestHealth(t *testing.T) {
 
 	expected := "You're good to...GO! :D"
@@ -16,10 +23,5 @@ func TestHealth(t *testing.T) {
 	}
 }
 
-// TestInvert - it should return a csv where rows and columns are reversed
-// Ok so, here's where I hit a wall, dig into http request testing or hand in the tech test?
-// will continue to do some research and will add once I learn more!
-func TestInvert(t *testing.T) {
-	input := []string{"a", "b", "c", "d"}
-	expected := fmt.Printf("%s\n", "a, c, b, d")
-}
+
+
